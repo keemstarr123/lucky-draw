15d5a28d56c54f19c270801ee6f36c87906241c0
keemstarr123 <122068716+keemstarr123@users.noreply.github.com>
2025-12-01 19:35:34 +0800

    Update prize items in constants.ts

--- constants.ts
+++ constants.ts
@@ -11,38 +11,31 @@ export const PRIZES: Prize[] = [
   },
   {
     id: '2',
-    name: 'Nintendo Switch',
-    image: 'https://images.unsplash.com/photo-1578303512597-81e6cc155b3e?auto=format&fit=crop&q=80&w=600',
+    name: 'Limited-edition Pickeball Paddle',
+    image: 'paddle.jpeg',
     color: 'from-red-500 to-red-800',
     rarity: 'Epic'
   },
   {
     id: '3',
-    name: 'Xbox Series X',
-    image: 'https://images.unsplash.com/photo-1621259182902-885f1c2a01e5?auto=format&fit=crop&q=80&w=600',
+    name: 'RM500 SportsDirect Gift Card',
+    image: 'sports.jpeg',
     color: 'from-green-500 to-green-900',
     rarity: 'Epic'
   },
   {
     id: '4',
-    name: 'iPhone 15 Pro',
-    image: 'https://images.unsplash.com/photo-1696446701796-da61225697cc?auto=format&fit=crop&q=80&w=600',
+    name: 'RM50 Grab Gift Card',
+    image: 'grab.jpeg',
     color: 'from-gray-700 to-black',
-    rarity: 'Legendary'
+    rarity: 'Common'
   },
   {
     id: '5',
-    name: '$500 Voucher',
-    image: 'https://images.unsplash.com/photo-1556742049-0cfed4f7a07d?auto=format&fit=crop&q=80&w=600',
+    name: 'Vip Court Exclusive Experience With Pros',
+    image: 'court.jpeg',
     color: 'from-yellow-400 to-orange-600',
     rarity: 'Rare'
-  },
-  {
-    id: '6',
-    name: 'Gaming Headset',
-    image: 'https://images.unsplash.com/photo-1612444530582-fc66183b16f7?auto=format&fit=crop&q=80&w=600',
-    color: 'from-purple-500 to-purple-900',
-    rarity: 'Common'
   },
 ];
 
